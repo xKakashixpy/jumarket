6f679215e391bfe5456d554b72334e336ee84b69
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-09-05 20:16:51 -0300

    Validacion Registro

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; // Importa ReactiveFormsModule
 
 import { IonicModule } from '@ionic/angular';
-
 import { RegisterPageRoutingModule } from './register-routing.module';
-
 import { RegisterPage } from './register.page';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule, // Agrega ReactiveFormsModule aquí
     IonicModule,
     RegisterPageRoutingModule
   ],
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  registerForm: FormGroup;
 
-  password: string = '';
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.registerForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      telefono: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*\d{4})(?=.*[a-z]{3}).{8,}$/)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
 
-  validatePassword() {
-    // Validar que la contraseña cumple con los requisitos
-    const regex = /^(?=.*[A-Z])(?=.*\d{4})(?=.*[a-z]{3}).{8,}$/;
-
-    if (regex.test(this.password)) {
-      console.log('Contraseña válida');
+  register() {
+    if (this.registerForm.valid) {
+      // El formulario es válido, puedes realizar cualquier otra lógica aquí
+      // Luego, navega a la página de inicio (reemplaza 'home' con tu ruta real)
+      this.router.navigate(['/home']);
     } else {
-      console.log('La contraseña no cumple con los requisitos.');
+      // El formulario no es válido, puedes mostrar un mensaje de error o realizar otras acciones
     }
   }
-
-  ngOnInit() {
-    // Aquí puedes realizar cualquier inicialización necesaria cuando se carga la página.
-  }
 }
+
+
+
+
+
