ca9d1ee18734ca5d4369b7362a05ec2d2b14cde3
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-09-05 20:25:47 -0300

    Animacion

--- src/app/forgot/forgot.page.ts
+++ src/app/forgot/forgot.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forgot',
   templateUrl: './forgot.page.html',
   styleUrls: ['./forgot.page.scss'],
 })
-export class ForgotPage implements OnInit {
+export class ForgotPage {
+  constructor(public alertController: AlertController) {}
 
-  constructor() { }
+  async showAlert() {
+    const alert = await this.alertController.create({
+      header: 'Hola',
+      subHeader: 'Revisa tu Correo',
+      message: 'Email de recuperación enviado',
+      buttons: ['OK'],
+    });
 
-  ngOnInit() {
+    await alert.present();
+
+    // Ocultar el mensaje después de 3 segundos
+    setTimeout(() => {
+      alert.dismiss();
+    }, 3000);
   }
-
 }
