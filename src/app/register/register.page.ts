0c7d4f2d984d60207f5d05f8b9eb533d1716c854
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2023-09-05 00:15:58 -0300

    Search

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  password: string = '';
 
-  ngOnInit() {
+  validatePassword() {
+    // Validar que la contraseña cumple con los requisitos
+    const regex = /^(?=.*[A-Z])(?=.*\d{4})(?=.*[a-z]{3}).{8,}$/;
+
+    if (regex.test(this.password)) {
+      console.log('Contraseña válida');
+    } else {
+      console.log('La contraseña no cumple con los requisitos.');
+    }
   }
 
+  ngOnInit() {
+    // Aquí puedes realizar cualquier inicialización necesaria cuando se carga la página.
+  }
 }
